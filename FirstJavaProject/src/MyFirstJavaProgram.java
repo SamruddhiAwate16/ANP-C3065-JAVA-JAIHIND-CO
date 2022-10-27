e91844fcbfe8753bc86c126d591bc3f48826ea91
Samruddhi <Samruddhi@DESKTOP-IPETC0Q>
2022-10-27 12:11:04 +0530

    Question1

--- FirstJavaProject/src/MyFirstJavaProgram.java
+++ FirstJavaProject/src/MyFirstJavaProgram.java
@@ -1,20 +1,20 @@
 
-public class findGreatest {
-
-	public static void main(int[] args) {
-		// TODO Auto-generated method stub
-		int a=10, b=20, c=55;
-		if(a>b && b<c) {
-			System.out.println("a is greatest number");
-		}
-		if(a<b && b>c) {
-			system.out.println("B is greatest number");
-		}
-		if(a==b && b==c) {
-			System.out.println("all numbers are same");
-		}
-		else {
-			System.out.println("c is greatest number");
-		
+public class MyFirstJavaProgram {
+ public  void setIntegerArray(int[] array) {
+	for(int i=0; i<array.length;i++) {
+		System.out.println("The data stored in array:" +array[i]);
 	}
-}
+	}
+ public void setInt(int a) {
+ }
+ public static void main(String[] args) {
+		int arr[]=new int [2];
+		arr[0]=1;
+		arr[1]=2;
+		int x=20;
+		
+		MyFirstJavaProgram  obj1 = new MyFirstJavaProgram();
+		obj1.setInt(x);
+		obj1.setIntegerArray(arr);
+		}
+	}
